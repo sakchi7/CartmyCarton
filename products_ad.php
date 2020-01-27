c6724f8e8399654a2b58e1b9842f0abbcd153612
Sakchi Lal <sakchilal@gmail.com>
2020-01-27 12:58:42 +0530

    An advertisement of products available

---
+++ products_ad.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/products.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Products</title>
+    
+    <style>
+    table{  
+      border: none;
+      text-align: left;
+    }
+
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+     td {
+      padding: 30px;
+      border : none;
+    }
+</style>
+    
+</head>
+
+<body>
+    <span><h1 align="center"><font style="CG Omega">Available  Products</font><img src="images/logomin2.png" align="right"></h1></span>
+    <div class="header" id="myHeader">
+    <div class="navbar">
+        <a  class="active" href="home.php"><i class="fa fa-fw fa-home"></i> Home</a>
+        <a href="myproduct.php"><i class="fa fa-fw fa-cart-plus"></i> My Products</a>
+        <a href="my_orders.php"><i class="fa fa-fw fa-cart-arrow-down"></i> My Orders</a>
+        <a href="front_page.php"><i class="fa fa-fw fa-user"></i> Logout</a></li>
+    </div>
+    <marquee direction="right"  behavior="alternate"  bgcolor="#FFE4E1" hspace="10" >
+        <font color = "#CA226B" size = "3"><b>Welcome to CartMyCarton!</b><i class="fa fa-fw fa-cart-arrow-down"></i></font> 
+    </marquee>
+    </div>
+</body>
+</html>
