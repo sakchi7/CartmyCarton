925cd5d92e3cf0df016c5efe5bea44ecbfa12ff1
Sakchi Lal <sakchilal@gmail.com>
2020-01-31 09:36:11 +0530

    Products sold by a user

--- myproduct.php
+++ myproduct.php
@@ -66,6 +66,213 @@
     		
     </div>
     </div>
+    
+    <table>
+ <?php
+    $user_num = $_SESSION['register_no'];
+$query = "SELECT * FROM products where owner_regno= '$user_num'";
+$result = mysqli_query($db,$query);
+if(mysqli_num_rows($result) == 0){
+  echo "<script type='text/javascript'>alert('You have not advertised any item')</script>";
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    
+    $id = $row["product_id"];
+    $buyer = $row["buyer_regno"];
+    
+        $query2 = "SELECT * FROM Users WHERE Registration_No = '$buyer'";
+    	$result2 = mysqli_query($db,$query2);
+    	$row2 = mysqli_fetch_assoc($result2);
+        $email = $row2["Email_id"];
+        $mobile = $row2["Mobile_no"];
+        $name = $row2["User_name"];
+        $regno = $row2["Registration_No"];
+    
+    if ($row["product_type"]==="Cooler") {
+      $query3 = "SELECT * FROM cooler WHERE cooler_id = '$id'";
+      $result3 = mysqli_query($db,$query3);
+      $row3 = mysqli_fetch_assoc($result3);
+         $image_name = $row3["image_path"];
+    if($buyer===NULL){
+        
+?>
+
+                <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <h2><u>Not Sold Yet</u></h2>
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <p><h4><b>Expected Amount : &#8377;<?php echo $row3["amount"]; ?></b></h4></p>
+<?php
+}
+        else{
+            
+?>
+
+       
+                    <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <h2><u>Buyer Details</u></h2>
+                        <p>Prodct Name: <b><?php echo $row["product_type"]; ?> </b></p>
+                            <p>Registration Number : <b><?php echo $buyer; ?></b></p>
+                             <p>Name : <b><?php echo $name; ?></b></p>
+                                <p>Mobile Number : <b><?php echo $mobile; ?></b></p>
+                                <p>Email Id : <b><?php echo $email; ?></b></p>
+                                
+                
+        <?php
+            }
+            }
+            else if ($row["product_type"]==="Kettle") {
+            $query2 = "SELECT * FROM kettle WHERE kettle_id = '$id'";
+            $result2 = mysqli_query($db,$query2);
+            $row3 = mysqli_fetch_assoc($result2);
+         $image_name = $row3["image_path"];
+         if($buyer===NULL)
+         {
+
+       ?>
+       
+         <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <h2><u>Not Sold Yet</u></h2>
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <p><h4><b>Expected Amount : &#8377;<?php echo $row3["amount"]; ?></b></h4></p>
+            <?php
+            }
+                    else{
+                        
+            ?>
+                   
+                        <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                           <h2><u>Buyer Details</u></h2>
+                        <p>Prodct Name: <b><?php echo $row["product_type"]; ?> </b></p>
+                            <p>Registration Number : <b><?php echo $buyer; ?></b></p>
+                             <p>Name : <b><?php echo $name; ?></b></p>
+                                <p>Mobile Number : <b><?php echo $mobile; ?></b></p>
+                                <p>Email Id : <b><?php echo $email; ?></b></p>
+                            
+        <?php
+        }
+          }
+            else if ($row["product_type"]==="Table") {
+            $query2 = "SELECT * FROM studytable WHERE table_id = '$id'";
+            $result2 = mysqli_query($db,$query2);
+            $row3 = mysqli_fetch_assoc($result2);
+             $image_name = $row3["image_path"];
+
+         if($buyer===NULL){  
+       ?>
+       
+          <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <h2><u>Not Sold Yet</u></h2>
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <p><h4><b>Expected Amount : &#8377;<?php echo $row3["amount"]; ?></b></h4></p>
+                <?php
+                }
+                        else{
+                            
+                ?>
+                          <tr align="center">
+                                <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                                <td align="left">
+                                <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                                   <h2><u>Buyer Details</u></h2>
+                        <p>Prodct Name: <b><?php echo $row["product_type"]; ?> </b></p>
+                            <p>Registration Number : <b><?php echo $buyer; ?></b></p>
+                             <p>Name : <b><?php echo $name; ?></b></p>
+                                <p>Mobile Number : <b><?php echo $mobile; ?></b></p>
+                                <p>Email Id : <b><?php echo $email; ?></b></p>
+    
+        <?php
+                }
+           }     
+            else if ($row["product_type"]==="Cycle") {
+            $query2 = "SELECT * FROM cycle WHERE cycle_id = '$id'";
+            $result2 = mysqli_query($db,$query2);
+            $row3 = mysqli_fetch_assoc($result2);
+             $image_name = $row3["image_path"];
+                if($buyer===NULL){
+           
+       ?>
+       
+         <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <h2><u>Not Sold Yet</u></h2>
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <p><h4><b>Expected Amount : &#8377;<?php echo $row3["amount"]; ?></b></h4></p>
+<?php
+}
+        else{
+            
+?>
+       
+                    <tr align="center">
+                            <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                            <td align="left">
+                            <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                                <h2><u>Buyer Details</u></h2>
+                        <p>Prodct Name: <b><?php echo $row["product_type"]; ?> </b></p>
+                            <p>Registration Number : <b><?php echo $buyer; ?></b></p>
+                             <p>Name : <b><?php echo $name; ?></b></p>
+                                <p>Mobile Number : <b><?php echo $mobile; ?></b></p>
+                                <p>Email Id : <b><?php echo $email; ?></b></p>
+<?php
+}}
+
+            else if ($row["product_type"]==="Bedroll") {
+            $query2 = "SELECT * FROM bedroll WHERE bed_id  = '$id'";
+            $result2 = mysqli_query($db,$query2);
+            $row3 = mysqli_fetch_assoc($result2);
+         $image_name = $row3["image_path"];
+        if($buyer===NULL)
+        {
+       ?>
+         <tr align="center">
+                        <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                        <td align="left">
+                        <h2><u>Not Sold Yet</u></h2>
+                        <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                        <p><h4><b>Expected Amount : &#8377;<?php echo $row3["amount"]; ?></b></h4></p>
+<?php
+}
+        else{
+            
+?>
+       
+       
+                                <tr align="center">
+                <td align="left"><?php echo "<img src='images/$image_name' style='width:400px;height:300px;'>";?></td>
+                <td align="left">
+                <p><h4><b>Product Id : <?php echo $id; ?></b></h4></p>
+                   <h2><u>Buyer Details</u></h2>
+                        <p>Prodct Name: <b><?php echo $row["product_type"]; ?> </b></p>
+                            <p>Registration Number : <b><?php echo $buyer; ?></b></p>
+                             <p>Name : <b><?php echo $name; ?></b></p>
+                                <p>Mobile Number : <b><?php echo $mobile; ?></b></p>
+                                <p>Email Id : <b><?php echo $email; ?></b></p>
+<?php
+} }      
+?> 
+        
+                                 
+                                           </td></tr>
+<?php
+}
+?> 
+
+                   
+ </table>   
   
       <footer class="container-fluid bg-4 " style="background:#FFE4E1;">
          <p ><center>For more queries, email us at: <a href="mailto: cartmycarton001@gmail.com" target="_blank">cartmycarton001@gmail.com</a></center></p>
